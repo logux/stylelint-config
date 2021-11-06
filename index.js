ecdc7fbf7b12e1e8d786779bf3c5dc92a589f65f
Andrey Sitnik <andrey@sitnik.ru>
2021-11-06 12:10:32 +0100

    Add Prettier config for editor

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 module.exports = {
-  extends: ["stylelint-config-standard", "stylelint-config-recess-order"],
+  extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
   rules: {
-    "declaration-block-trailing-semicolon": null,
-    "block-closing-brace-newline-before": null,
-    "custom-property-empty-line-before": null,
-    "selector-pseudo-class-no-unknown": [
+    'declaration-block-trailing-semicolon': null,
+    'block-closing-brace-newline-before': null,
+    'custom-property-empty-line-before': null,
+    'selector-pseudo-class-no-unknown': [
       true,
       {
-        ignorePseudoClasses: ["global"],
-      },
+        ignorePseudoClasses: ['global']
+      }
     ],
-    "block-opening-brace-space-before": null,
-    "declaration-colon-newline-after": null,
-    "selector-class-pattern": null,
-    "at-rule-no-unknown": [
+    'block-opening-brace-space-before': null,
+    'declaration-colon-newline-after': null,
+    'selector-class-pattern': null,
+    'at-rule-no-unknown': [
       true,
       {
-        ignoreAtRules: ["define-mixin", "mixin"],
-      },
+        ignoreAtRules: ['define-mixin', 'mixin']
+      }
     ],
-    "color-named": "never",
-  },
-};
+    'color-named': 'never'
+  }
+}
