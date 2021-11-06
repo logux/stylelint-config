8f279300471de1902645fa836ab04ceba82a2b18
Andrey Sitnik <andrey@sitnik.ru>
2021-11-06 12:09:44 +0100

    Disable selector-class-pattern

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-recess-order'
-  ],
+  extends: ["stylelint-config-standard", "stylelint-config-recess-order"],
   rules: {
-    'declaration-block-trailing-semicolon': null,
-    'block-closing-brace-newline-before': null,
-    'custom-property-empty-line-before': null,
-    'block-opening-brace-space-before': null,
+    "declaration-block-trailing-semicolon": null,
+    "block-closing-brace-newline-before": null,
+    "custom-property-empty-line-before": null,
+    "selector-pseudo-class-no-unknown": [
+      true,
+      {
+        ignorePseudoClasses: ["global"],
+      },
+    ],
+    "block-opening-brace-space-before": null,
     "declaration-colon-newline-after": null,
-    "selector-pseudo-class-no-unknown": [true, {
-      "ignorePseudoClasses": ["global"]
-    }],
-    'at-rule-no-unknown': [true, {
-      ignoreAtRules: [
-        'define-mixin',
-        'mixin'
-      ]
-    }],
-    'color-named': 'never'
-  }
-}
+    "selector-class-pattern": null,
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: ["define-mixin", "mixin"],
+      },
+    ],
+    "color-named": "never",
+  },
+};
