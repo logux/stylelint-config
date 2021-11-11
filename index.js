34618ee7d1aeb314fc6f26947c28d46db7b3fb1a
Andrey Sitnik <andrey@sitnik.ru>
2021-11-11 12:37:33 +0100

    Add color rules

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
   rules: {
+    'declaration-property-value-disallowed-list': {},
     'declaration-block-trailing-semicolon': null,
     'block-closing-brace-newline-before': null,
     'custom-property-empty-line-before': null,
+    'block-opening-brace-space-before': 'always',
     'selector-pseudo-class-no-unknown': [
       true,
       {
@@ -12,13 +14,18 @@ module.exports = {
     ],
     'block-opening-brace-space-before': null,
     'declaration-colon-newline-after': null,
+    'property-no-vendor-prefix': null,
+    'function-disallowed-list': ['rgb', 'rgba', 'hsl', 'hsla'],
+    'color-function-notation': 'modern',
     'selector-class-pattern': null,
+    'alpha-value-notation': 'number',
+    'hue-degree-notation': 'angle',
     'at-rule-no-unknown': [
       true,
       {
         ignoreAtRules: ['define-mixin', 'mixin']
       }
     ],
-    'color-named': 'never'
+    'color-no-hex': true
   }
 }
