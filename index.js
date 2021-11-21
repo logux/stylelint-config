4c5bb64fb2f540c3e31a503ed1d2d7da428ce8c7
Andrey Sitnik <andrey@sitnik.ru>
2021-11-21 20:31:31 +0100

    Remove no-irregular-whitespace and function-url-quotes rules

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ module.exports = {
     'property-no-vendor-prefix': null,
     'function-disallowed-list': ['rgba', 'hsla'],
     'color-function-notation': 'modern',
+    'no-irregular-whitespace': null,
     'selector-class-pattern': null,
     'alpha-value-notation': 'number',
+    'function-url-quotes': null,
     'hue-degree-notation': 'number',
     'at-rule-no-unknown': [
       true,
