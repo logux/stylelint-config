4ff04a065c0de94790f4a850255ec13b2a832ce3
Andrey Sitnik <andrey@sitnik.ru>
2022-06-06 08:52:16 +0200

    Clean up config with MDN update with oklch()

--- index.js
+++ index.js
@@ -15,12 +15,6 @@ module.exports = {
     'no-irregular-whitespace': null,
     'selector-class-pattern': null,
     'alpha-value-notation': 'number',
-    'function-no-unknown': [
-      true,
-      {
-        ignoreFunctions: ['oklch']
-      }
-    ],
     'function-url-quotes': null,
     'hue-degree-notation': 'number',
     'at-rule-no-unknown': [
