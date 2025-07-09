b43a3d05c220906cb7fce8755694f71ccba5d190
Andrey Sitnik <andrey@sitnik.ru>
2025-07-09 16:39:30 +0000

    Order keys by my config

--- index.js
+++ index.js
@@ -2,51 +2,51 @@ let propertyGroups = require('stylelint-config-recess-order/groups')
 
 module.exports = {
   extends: ['stylelint-config-standard'],
+  overrides: [
+    {
+      files: ['**/*.sss'],
+      rules: {
+        'block-closing-brace-newline-before': null,
+        'block-opening-brace-space-before': null,
+        'custom-property-empty-line-before': null,
+        'declaration-block-trailing-semicolon': null,
+        'declaration-colon-newline-after': null
+      }
+    }
+  ],
   plugins: ['stylelint-gamut', 'stylelint-order'],
   rules: {
-    'declaration-property-value-disallowed-list': {},
-    'keyframe-block-no-duplicate-selectors': true,
-    'selector-pseudo-class-no-unknown': [
-      true,
-      {
-        ignorePseudoClasses: ['global']
-      }
-    ],
-    'gamut/color-no-out-gamut-range': true,
-    'property-no-vendor-prefix': null,
-    'function-disallowed-list': ['rgba', 'hsla'],
-    'color-function-notation': 'modern',
-    'no-irregular-whitespace': null,
-    'keyframes-name-pattern': [
-      '^--([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
-      {
-        message: name => `Expected keyframe name ${name} to be --kebab-case`
-      }
-    ],
-    'order/properties-order': [propertyGroups.default, { severity: 'warning' }],
-    'selector-class-pattern': null,
     'alpha-value-notation': 'number',
-    'function-url-quotes': null,
-    'hue-degree-notation': 'number',
-    'lightness-notation': 'number',
     'at-rule-no-unknown': [
       true,
       {
         ignoreAtRules: ['define-mixin', 'mixin']
       }
     ],
-    'color-no-hex': true
-  },
-  overrides: [
-    {
-      files: ['**/*.sss'],
-      rules: {
-        'declaration-block-trailing-semicolon': null,
-        'block-closing-brace-newline-before': null,
-        'custom-property-empty-line-before': null,
-        'block-opening-brace-space-before': null,
-        'declaration-colon-newline-after': null
+    'color-function-notation': 'modern',
+    'color-no-hex': true,
+    'declaration-property-value-disallowed-list': {},
+    'function-disallowed-list': ['rgba', 'hsla'],
+    'function-url-quotes': null,
+    'gamut/color-no-out-gamut-range': true,
+    'hue-degree-notation': 'number',
+    'keyframe-block-no-duplicate-selectors': true,
+    'keyframes-name-pattern': [
+      '^--([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
+      {
+        message: name => `Expected keyframe name ${name} to be --kebab-case`
       }
-    }
-  ]
+    ],
+    'lightness-notation': 'number',
+    'no-irregular-whitespace': null,
+    'order/properties-order': [propertyGroups.default, { severity: 'warning' }],
+    'property-no-vendor-prefix': null,
+    'selector-class-pattern': null,
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global']
+      }
+    ]
+  }
 }
